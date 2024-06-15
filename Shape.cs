4d929bb9817f38498c4bf9182a885691cdf761f0
K-andy13 <kmanderson@st.ug.edu.gh>
2024-06-15 17:58:26 +0000

    Abstract Classesa and Methods

---
+++ Shape.cs
@@ -0,0 +1,12 @@
+using System;
+
+public abstract class Shape
+{
+    public abstract void GetArea();
+
+    class Circle : Shape
+    {
+        public override void (decimal r) {
+ Console.WriteLine("The area of this circle is:"+ Math.PI*r*r);}
+}
+}
