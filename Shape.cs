72fbfd49f59d6b20c7f3df3add203a899a465efa
K-andy13 <102968948+K-andy13@users.noreply.github.com>
2024-06-15 18:00:46 +0000

    Abstract Classes and Methods

--- Shape.cs
+++ Shape.cs
@@ -1,12 +1,47 @@
-using System;
-
 public abstract class Shape
 {
-    public abstract void GetArea();
+    public abstract double GetArea();
 
-    class Circle : Shape
+    public class Circle : Shape
     {
-        public override void (decimal r) {
- Console.WriteLine("The area of this circle is:"+ Math.PI*r*r);}
-}
-}
+        private double radius;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double w, double h)
+        {
+            width = w;
+            height = h;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+    static void Main()
+    {
+        #region Abstract Classes and Methods
+        Shape myCircle = new Circle(5.0);
+        Console.WriteLine("Area of the circle: " + myCircle.GetArea());
+
+        Shape myRectangle = new Rectangle(4.0, 6.0);
+        Console.WriteLine("Area of the rectangle: " + myRectangle.GetArea());
+    }
+
+
